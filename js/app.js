e69cc953f327b40d3a1bcde2c7f46f06c129002e
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-01-11 17:51:14 -0800

    commented out all console.logs

--- js/app.js
+++ js/app.js
@@ -16,61 +16,61 @@ let wife = 'I am married to my wonderful wife!'
 
 let answerOne = prompt('Did I live in Florida?').toLocaleLowerCase();
 if(answerOne === 'yes' || answerOne === 'y') {
-  console.log(`${correct} ${florida}`);
+  //console.log(`${correct} ${florida}`);
   alert(`${correct} ${florida}`);
 }else if(answerOne === 'no' || answerOne === 'n'){
-  console.log(`${nope}${florida}`);
+  //console.log(`${nope}${florida}`);
   alert(`${nope}${florida}`);
 }else{
-  console.log(noDirections);
+  //console.log(noDirections);
   alert(noDirections);
 }
 
 let answerTwo = prompt('Was I a Cheesemaker?').toLocaleLowerCase();
 if(answerTwo === 'yes' || answerTwo === 'y') {
-  console.log(`${correct} ${cheese}`);
+  //console.log(`${correct} ${cheese}`);
   alert(`${correct} ${cheese}`);
 }else if(answerTwo === 'no' || answerTwo === 'n'){
-  console.log(`${nope} ${cheese}`);
+  //console.log(`${nope} ${cheese}`);
   alert(`${nope} ${cheese}`);
 }else{
-  console.log(noDirections);
+  //console.log(noDirections);
   alert(noDirections);
 }
 
 let answerThree = prompt('Did I go to the University of Washington?').toLocaleLowerCase();
 if(answerThree === 'no' || answerThree === 'n') {
-  console.log(`${correct} ${ucf}`);
+  //console.log(`${correct} ${ucf}`);
   alert(`${correct} ${ucf}`);
 }else if(answerThree === 'yes' || answerThree === 'y'){
-  console.log(`${nope} ${ucf}`);
+  //console.log(`${nope} ${ucf}`);
   alert(`${nope} ${ucf}`);
 }else{
-  console.log(noDirections);
+  //console.log(noDirections);
   alert(noDirections);
 }
 
 let answerFour = prompt('Was my Last Career as a Scuba Instructor?').toLocaleLowerCase();
 if(answerFour === 'no' || answerFour === 'n') {
-  console.log(`${correct} ${scuba}`);
+  //console.log(`${correct} ${scuba}`);
   alert(`${correct} ${scuba}`);
-  }else if(answerFour === 'yes' || answerFour === 'y'){
-  console.log(`${nope} ${scuba}`);
+}else if(answerFour === 'yes' || answerFour === 'y'){
+  //console.log(`${nope} ${scuba}`);
   alert(`${nope} ${scuba}`);
 }else{
-  console.log(noDirections);
+  //console.log(noDirections);
   alert(noDirections);
 }
 
 let answerFive = prompt('Am I married?').toLocaleLowerCase();
 if(answerFive === 'yes' || answerFive === 'y') {
-  console.log(`${correct} ${wife}`);
+  //console.log(`${correct} ${wife}`);
   alert(`${correct} ${wife}`);
 }else if(answerFive === 'no' || answerFive === 'n'){
-  console.log(`${nope} ${wife}`);
+  //console.log(`${nope} ${wife}`);
   alert(`${nope} ${wife}`);
 }else{
-  console.log(noDirections);
+  //console.log(noDirections);
   alert(noDirections);
 } 
 
