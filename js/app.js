7917b8ee325f28aad4318559451f729d5702a641
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-01-12 19:12:53 -0800

    Created 7th question(multi answer)

--- js/app.js
+++ js/app.js
@@ -1,78 +1,83 @@
 'use strict';
 
-
+let userCorrect = 0;
 
 let user = prompt('What is your name?');
 alert(`Welcome ${user} to my About Me page!`);
-// alert(`${user} ! lets see how much you know about me \n Answer the following questions with a yes or a no`);
-// let correct = (`Thats correct ${user}! `);
-// let noDirections = 'Come on now, read the instructions \n answer yes or no!';
-// let florida = 'I lived in Florida for almost a decade!';
-// let nope = (`Sorry thats wrong ${user}! `);
-// let cheese =  'I was!! I pasteurized milk and everything!';
-// let ucf = 'I went to the University of Central Florida \n UCF UCF UCF UCF UCF!';
-// let scuba = 'I hate going deep under water! I was an electrician!';
-// let wife = 'I am married to my wonderful wife!';
+alert(`${user} ! lets see how much you know about me \n Answer the following questions with a yes or a no`);
+let correct = (`Thats correct ${user}! `);
+let noDirections = 'Come on now, read the instructions \n answer yes or no!';
+let florida = 'I lived in Florida for almost a decade!';
+let nope = (`Sorry thats wrong ${user}! `);
+let cheese =  'I was!! I pasteurized milk and everything!';
+let ucf = 'I went to the University of Central Florida \n UCF UCF UCF UCF UCF!';
+let scuba = 'I hate going deep under water! I was an electrician!';
+let wife = 'I am married to my wonderful wife!';
 
-// let answerOne = prompt('Did I live in Florida?').toLocaleLowerCase();
-// if(answerOne === 'yes' || answerOne === 'y') {
-//   //console.log(`${correct} ${florida}`);
-//   alert(`${correct} ${florida}`);
-// }else if(answerOne === 'no' || answerOne === 'n'){
-//   //console.log(`${nope}${florida}`);
-//   alert(`${nope}${florida}`);
-// }else{
-//   //console.log(noDirections);
-//   alert(noDirections);
-// }
+let answerOne = prompt('Did I live in Florida?').toLocaleLowerCase();
+if(answerOne === 'yes' || answerOne === 'y') {
+  //console.log(`${correct} ${florida}`);
+  userCorrect++;
+  alert(`${correct} ${florida}`);
+}else if(answerOne === 'no' || answerOne === 'n'){
+  //console.log(`${nope}${florida}`);
+  alert(`${nope}${florida}`);
+}else{
+  //console.log(noDirections);
+  alert(noDirections);
+}
 
-// let answerTwo = prompt('Was I a Cheesemaker?').toLocaleLowerCase();
-// if(answerTwo === 'yes' || answerTwo === 'y') {
-//   //console.log(`${correct} ${cheese}`);
-//   alert(`${correct} ${cheese}`);
-// }else if(answerTwo === 'no' || answerTwo === 'n'){
-//   //console.log(`${nope} ${cheese}`);
-//   alert(`${nope} ${cheese}`);
-// }else{
-//   //console.log(noDirections);
-//   alert(noDirections);
-// }
+let answerTwo = prompt('Was I a Cheesemaker?').toLocaleLowerCase();
+if(answerTwo === 'yes' || answerTwo === 'y') {
+  //console.log(`${correct} ${cheese}`);
+  userCorrect++;
+  alert(`${correct} ${cheese}`);
+}else if(answerTwo === 'no' || answerTwo === 'n'){
+  //console.log(`${nope} ${cheese}`);
+  alert(`${nope} ${cheese}`);
+}else{
+  //console.log(noDirections);
+  alert(noDirections);
+}
 
-// let answerThree = prompt('Did I go to the University of Washington?').toLocaleLowerCase();
-// if(answerThree === 'no' || answerThree === 'n') {
-//   //console.log(`${correct} ${ucf}`);
-//   alert(`${correct} ${ucf}`);
-// }else if(answerThree === 'yes' || answerThree === 'y'){
-//   //console.log(`${nope} ${ucf}`);
-//   alert(`${nope} ${ucf}`);
-// }else{
-//   //console.log(noDirections);
-//   alert(noDirections);
-// }
+let answerThree = prompt('Did I go to the University of Washington?').toLocaleLowerCase();
+if(answerThree === 'no' || answerThree === 'n') {
+  //console.log(`${correct} ${ucf}`);
+  alert(`${correct} ${ucf}`);
+  userCorrect++;
+}else if(answerThree === 'yes' || answerThree === 'y'){
+  //console.log(`${nope} ${ucf}`);
+  alert(`${nope} ${ucf}`);
+}else{
+  //console.log(noDirections);
+  alert(noDirections);
+}
 
-// let answerFour = prompt('Was my Last Career as a Scuba Instructor?').toLocaleLowerCase();
-// if(answerFour === 'no' || answerFour === 'n') {
-//   //console.log(`${correct} ${scuba}`);
-//   alert(`${correct} ${scuba}`);
-// }else if(answerFour === 'yes' || answerFour === 'y'){
-//   //console.log(`${nope} ${scuba}`);
-//   alert(`${nope} ${scuba}`);
-// }else{
-//   //console.log(noDirections);
-//   alert(noDirections);
-// }
+let answerFour = prompt('Was my Last Career as a Scuba Instructor?').toLocaleLowerCase();
+if(answerFour === 'no' || answerFour === 'n') {
+  //console.log(`${correct} ${scuba}`);
+  userCorrect++;
+  alert(`${correct} ${scuba}`);
+}else if(answerFour === 'yes' || answerFour === 'y'){
+  //console.log(`${nope} ${scuba}`);
+  alert(`${nope} ${scuba}`);
+}else{
+  //console.log(noDirections);
+  alert(noDirections);
+}
 
-// let answerFive = prompt('Am I married?').toLocaleLowerCase();
-// if(answerFive === 'yes' || answerFive === 'y') {
-//   //console.log(`${correct} ${wife}`);
-//   alert(`${correct} ${wife}`);
-// }else if(answerFive === 'no' || answerFive === 'n'){
-//   //console.log(`${nope} ${wife}`);
-//   alert(`${nope} ${wife}`);
-// }else{
-//   //console.log(noDirections);
-//   alert(noDirections);
-// } 
+let answerFive = prompt('Am I married?').toLocaleLowerCase();
+if(answerFive === 'yes' || answerFive === 'y') {
+  //console.log(`${correct} ${wife}`);
+  userCorrect++;
+  alert(`${correct} ${wife}`);
+}else if(answerFive === 'no' || answerFive === 'n'){
+  //console.log(`${nope} ${wife}`);
+  alert(`${nope} ${wife}`);
+}else{
+  //console.log(noDirections);
+  alert(noDirections);
+} 
 
 let tryCount = 4;
 let mango = 6;
@@ -83,6 +88,7 @@ while(tryCount){
   let answerSix = prompt(`${user}, How many Mangos are on my counter right now??`);
   console.log(typeof(answerSix));
   if(parseInt(answerSix) === mango){
+    userCorrect++;
     alert(`Great job ${user} you got it right! \n${rMango}`);break;
   }
   else if(answerSix > mango){
@@ -90,7 +96,6 @@ while(tryCount){
   }else {
     alert(`too low ${user} try again`);
   }
-  
   tryCount--;
   console.log(tryCount);
   if(tryCount === 0) {alert(`Sorry ${user}, All out of tries\n${rMango}`);
@@ -99,6 +104,28 @@ while(tryCount){
 
 
 
+let topMovies = ['memento', 'grand hotel budapest', 'princess mononoke','your name','spirited away', 'lord of the rings','howl\'s moving castle','the matrix','enter the dragon','snatch'];
 
+let tryCount2 = 6;
 
-// alert(`Thanks for playing ${user}, enjoy my page!`)
+circle: while(tryCount2){
+  let answerSeven = prompt('Try to guess one of my top ten favorite movies!\n little hint, I love me some anime ');
+  console.log(answerSeven);
+  for(let i = 0; i < topMovies.length; i++){
+    console.log(i);
+    if(answerSeven === topMovies[i]){
+      alert('right');
+      userCorrect++;
+      tryCount2 = 1;
+      break circle;
+    }else{
+      answerSeven = alert('Wrong answer!');break;
+    }
+  }
+  tryCount2--;
+  console.log(tryCount2);
+  if(tryCount2 === 0) {alert(`Sorry ${user}, All out of tries\n add list of movies`);}
+}
+
+console.log('correct answers' + userCorrect);
+alert(userCorrect);
