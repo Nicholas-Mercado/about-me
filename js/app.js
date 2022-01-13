e864bfc98de9af831154d05858769be560c4cf63
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-01-12 16:24:31 -0800

    Created a sixth question(number game)

--- js/app.js
+++ js/app.js
@@ -4,74 +4,101 @@
 
 let user = prompt('What is your name?');
 alert(`Welcome ${user} to my About Me page!`);
-alert(`${user} ! lets see how much you know about me \n Answer the following questions with a yes or a no`);
-let correct = (`Thats correct ${user}! `)
-let noDirections = 'Come on now, read the instructions \n answer yes or no!'
-let florida = 'I lived in Florida for almost a decade!'
-let nope = (`Sorry thats wrong ${user}! `)
-let cheese =  'I was!! I pasteurized milk and everything!'
-let ucf = 'I went to the University of Central Florida \n UCF UCF UCF UCF UCF!'
-let scuba = 'I hate going deep under water! I was an electrician!'
-let wife = 'I am married to my wonderful wife!'
+// alert(`${user} ! lets see how much you know about me \n Answer the following questions with a yes or a no`);
+// let correct = (`Thats correct ${user}! `);
+// let noDirections = 'Come on now, read the instructions \n answer yes or no!';
+// let florida = 'I lived in Florida for almost a decade!';
+// let nope = (`Sorry thats wrong ${user}! `);
+// let cheese =  'I was!! I pasteurized milk and everything!';
+// let ucf = 'I went to the University of Central Florida \n UCF UCF UCF UCF UCF!';
+// let scuba = 'I hate going deep under water! I was an electrician!';
+// let wife = 'I am married to my wonderful wife!';
 
-let answerOne = prompt('Did I live in Florida?').toLocaleLowerCase();
-if(answerOne === 'yes' || answerOne === 'y') {
-  //console.log(`${correct} ${florida}`);
-  alert(`${correct} ${florida}`);
-}else if(answerOne === 'no' || answerOne === 'n'){
-  //console.log(`${nope}${florida}`);
-  alert(`${nope}${florida}`);
-}else{
-  //console.log(noDirections);
-  alert(noDirections);
+// let answerOne = prompt('Did I live in Florida?').toLocaleLowerCase();
+// if(answerOne === 'yes' || answerOne === 'y') {
+//   //console.log(`${correct} ${florida}`);
+//   alert(`${correct} ${florida}`);
+// }else if(answerOne === 'no' || answerOne === 'n'){
+//   //console.log(`${nope}${florida}`);
+//   alert(`${nope}${florida}`);
+// }else{
+//   //console.log(noDirections);
+//   alert(noDirections);
+// }
+
+// let answerTwo = prompt('Was I a Cheesemaker?').toLocaleLowerCase();
+// if(answerTwo === 'yes' || answerTwo === 'y') {
+//   //console.log(`${correct} ${cheese}`);
+//   alert(`${correct} ${cheese}`);
+// }else if(answerTwo === 'no' || answerTwo === 'n'){
+//   //console.log(`${nope} ${cheese}`);
+//   alert(`${nope} ${cheese}`);
+// }else{
+//   //console.log(noDirections);
+//   alert(noDirections);
+// }
+
+// let answerThree = prompt('Did I go to the University of Washington?').toLocaleLowerCase();
+// if(answerThree === 'no' || answerThree === 'n') {
+//   //console.log(`${correct} ${ucf}`);
+//   alert(`${correct} ${ucf}`);
+// }else if(answerThree === 'yes' || answerThree === 'y'){
+//   //console.log(`${nope} ${ucf}`);
+//   alert(`${nope} ${ucf}`);
+// }else{
+//   //console.log(noDirections);
+//   alert(noDirections);
+// }
+
+// let answerFour = prompt('Was my Last Career as a Scuba Instructor?').toLocaleLowerCase();
+// if(answerFour === 'no' || answerFour === 'n') {
+//   //console.log(`${correct} ${scuba}`);
+//   alert(`${correct} ${scuba}`);
+// }else if(answerFour === 'yes' || answerFour === 'y'){
+//   //console.log(`${nope} ${scuba}`);
+//   alert(`${nope} ${scuba}`);
+// }else{
+//   //console.log(noDirections);
+//   alert(noDirections);
+// }
+
+// let answerFive = prompt('Am I married?').toLocaleLowerCase();
+// if(answerFive === 'yes' || answerFive === 'y') {
+//   //console.log(`${correct} ${wife}`);
+//   alert(`${correct} ${wife}`);
+// }else if(answerFive === 'no' || answerFive === 'n'){
+//   //console.log(`${nope} ${wife}`);
+//   alert(`${nope} ${wife}`);
+// }else{
+//   //console.log(noDirections);
+//   alert(noDirections);
+// } 
+
+let tryCount = 4;
+let mango = 6;
+let rMango = (`I have ${mango} mangos on my counter`);
+
+
+while(tryCount){
+  let answerSix = prompt(`${user}, How many Mangos are on my counter right now??`);
+  console.log(typeof(answerSix));
+  if(parseInt(answerSix) === mango){
+    alert(`Great job ${user} you got it right! \n${rMango}`);break;
+  }
+  else if(answerSix > mango){
+    alert(`too high ${user} try again`);
+  }else {
+    alert(`too low ${user} try again`);
+  }
+  
+  tryCount--;
+  console.log(tryCount);
+  if(tryCount === 0) {alert(`Sorry ${user}, All out of tries\n${rMango}`);
+  }
 }
 
-let answerTwo = prompt('Was I a Cheesemaker?').toLocaleLowerCase();
-if(answerTwo === 'yes' || answerTwo === 'y') {
-  //console.log(`${correct} ${cheese}`);
-  alert(`${correct} ${cheese}`);
-}else if(answerTwo === 'no' || answerTwo === 'n'){
-  //console.log(`${nope} ${cheese}`);
-  alert(`${nope} ${cheese}`);
-}else{
-  //console.log(noDirections);
-  alert(noDirections);
-}
 
-let answerThree = prompt('Did I go to the University of Washington?').toLocaleLowerCase();
-if(answerThree === 'no' || answerThree === 'n') {
-  //console.log(`${correct} ${ucf}`);
-  alert(`${correct} ${ucf}`);
-}else if(answerThree === 'yes' || answerThree === 'y'){
-  //console.log(`${nope} ${ucf}`);
-  alert(`${nope} ${ucf}`);
-}else{
-  //console.log(noDirections);
-  alert(noDirections);
-}
 
-let answerFour = prompt('Was my Last Career as a Scuba Instructor?').toLocaleLowerCase();
-if(answerFour === 'no' || answerFour === 'n') {
-  //console.log(`${correct} ${scuba}`);
-  alert(`${correct} ${scuba}`);
-}else if(answerFour === 'yes' || answerFour === 'y'){
-  //console.log(`${nope} ${scuba}`);
-  alert(`${nope} ${scuba}`);
-}else{
-  //console.log(noDirections);
-  alert(noDirections);
-}
 
-let answerFive = prompt('Am I married?').toLocaleLowerCase();
-if(answerFive === 'yes' || answerFive === 'y') {
-  //console.log(`${correct} ${wife}`);
-  alert(`${correct} ${wife}`);
-}else if(answerFive === 'no' || answerFive === 'n'){
-  //console.log(`${nope} ${wife}`);
-  alert(`${nope} ${wife}`);
-}else{
-  //console.log(noDirections);
-  alert(noDirections);
-} 
 
-alert(`Thanks for playing ${user}, enjoy my page!`)
+// alert(`Thanks for playing ${user}, enjoy my page!`)
